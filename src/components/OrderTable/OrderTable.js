a2c4a3898ef2276ca22eb2d570a97a8aedd93afe
Zeynep Dündar <dundarzey@itu.edu.tr>
2022-07-05 18:43:22 +0300

    Create data grid with mock data

--- src/components/OrderTable/OrderTable.js
+++ src/components/OrderTable/OrderTable.js
@@ -1,41 +1,70 @@
-import * as React from "react";
+import { useState } from "react";
 import {
   DataGrid,
   GridToolbarContainer,
   GridToolbarFilterButton,
 } from "@mui/x-data-grid";
-import { useDemoData } from "@mui/x-data-grid-generator";
+
+const CustomToolbar = ({ setFilterButtonEl }) => (
+  <GridToolbarContainer>
+    <GridToolbarFilterButton ref={setFilterButtonEl} />
+  </GridToolbarContainer>
+);
+
+const columns = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 110,
+    editable: true,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
 
 const OrderTable = ({ items }) => {
   console.log(items);
 
-  const VISIBLE_FIELDS = [
-    "name",
-    "rating",
-    "country",
-    "dateCreated",
-    "isAdmin",
-  ];
-
-  const CustomToolbar = ({ setFilterButtonEl }) => (
-    <GridToolbarContainer>
-      <GridToolbarFilterButton ref={setFilterButtonEl} />
-    </GridToolbarContainer>
-  );
-
-  const { data } = useDemoData({
-    dataSet: "Employee",
-    visibleFields: VISIBLE_FIELDS,
-    rowLength: 100,
-  });
-
-  const [filterButtonEl, setFilterButtonEl] =
-    (React.useState < HTMLButtonElement) | (null > null);
+  const [filterButtonEl, setFilterButtonEl] = useState(null);
 
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <div style={{ height: 400, width: "50%" }}>
       <DataGrid
-        {...data}
+        rows={rows}
+        columns={columns}
         components={{
           Toolbar: CustomToolbar,
         }}
